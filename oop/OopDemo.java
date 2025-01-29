511c995355dd5ab4687885ae305191bbd6be3cb3
shakhawat bhuiyan <princebhuiyan786@yahoo.com>
2025-01-28 21:29:17 -0500

    package name oop class name oop demo have been created

---
+++ oop/OopDemo.java
@@ -0,0 +1,24 @@
+package array.oop;
+
+public class OopDemo {
+
+
+    public static void janiNa(){
+        System.out.println("ki kormo");
+    }
+
+
+    public static void main(String[] args) {
+
+
+        OopDemo obj = new OopDemo();
+
+        obj.janiNa();
+
+
+
+    }
+
+
+
+}
