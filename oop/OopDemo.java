a3279a6f31f3f032ec96582f24fe0dc2330eaeb8
shakhawat bhuiyan <princebhuiyan786@yahoo.com>
2025-01-28 21:30:41 -0500

    oopDemo Modiffed package name oop class name oop demo have been created

--- oop/OopDemo.java
+++ oop/OopDemo.java
@@ -3,22 +3,8 @@ package array.oop;
 public class OopDemo {
 
 
-    public static void janiNa(){
-        System.out.println("ki kormo");
-    }
-
-
-    public static void main(String[] args) {
-
-
-        OopDemo obj = new OopDemo();
-
-        obj.janiNa();
-
-
-
     }
 
 
 
-}
+
